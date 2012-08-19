d114331c5ff7eb8d56fc479e9ddd231bcc154a89
Dennis Keefe <dkeefe75@gmail.com>
2012-08-18 21:43:44 -0500

    First run at twitter bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Storex::Application.config.secret_token = 'bc4998bac5d625273b966487ed0e1a64e3618d98d4573eef8fb0e979830406997df309fe4d6193a2f1a0f943458603c3aa8194eb9659b5e653e26ea528d00ef2'
